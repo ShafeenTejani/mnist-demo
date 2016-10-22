731465b1f88a6d8353f99bd77310b16acc4ab285
Shafeen Tejani <shafeen.tejani@gmail.com>
2016-10-22 17:26:08 +0100

    Update 28x28 canvas with a re-scaled version of the input

--- web/app/components/InputCanvas.js
+++ web/app/components/InputCanvas.js
@@ -6,6 +6,9 @@ class InputCanvas extends React.Component {
   componentDidMount() {
     this.canvas = findDOMNode(this.refs.inputCanvas);
     this.ctx = this.canvas.getContext('2d');
+
+    this.rescaledCanvas = findDOMNode(this.refs.rescaledCanvas);
+    this.rescaledCtx = this.rescaledCanvas.getContext('2d');
   }
 
   constructor(props) {
@@ -16,6 +19,7 @@ class InputCanvas extends React.Component {
     this.getCursorPosition = this.getCursorPosition.bind(this);
     this.drawLine = this.drawLine.bind(this);
     this.clear = this.clear.bind(this);
+    this.inputDrawn = this.inputDrawn.bind(this);
     this.state = {
       currentPosition: null,
       drawing: false
@@ -34,6 +38,7 @@ class InputCanvas extends React.Component {
     const currentPosition = this.getCursorPosition(e);
 
     this.drawLine(previousPosition, currentPosition);
+    this.inputDrawn();
 
     this.setState({currentPosition: currentPosition});
   }
@@ -64,20 +69,60 @@ class InputCanvas extends React.Component {
   }
 
   clear() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.rescaledCtx.clearRect(0, 0, this.rescaledCanvas.width, this.rescaledCanvas.height);
+  }
+
+  inputDrawn() {
+    this.rescaledCtx.clearRect(0, 0, this.rescaledCanvas.width, this.rescaledCanvas.height);
+    const imageData = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height).data;
+
+    const pixelIntensities = Array(imageData.length / 4); //RGBA
+
+    for (let i=0; i < imageData.length; i=i+4) {
+      pixelIntensities[i/4] = imageData[i+3];
+    }
+
+    const rescaled = Array(28**2);
+
+    for (let i=0; i < rescaled.length; i++) {
+      let xStart = (i%28)*8;
+      let xEnd = xStart + 8;
+      let yStart = parseInt(i/28) * 8;
+      let yEnd = yStart + 8;
+
+      let pixelSum = 0;
+      for (let x=xStart; x < xEnd; x++) {
+        for (let y=yStart; y < yEnd; y++) {
+          pixelSum += pixelIntensities[x + y*224]
+        }
+      }
+
+      rescaled[i] = pixelSum / 64.0;
+    }
+
+    for (let i=0; i < rescaled.length; i++) {
+      this.rescaledCtx.fillStyle = "rgba(0,0,0,"+(rescaled[i]/255)+")";
+      let x = i % 28;
+      let y = i / 28;
+      this.rescaledCtx.fillRect( x, y, 1, 1 );
+    }
   }
 
   render() {
     return <div className="input-canvas-container">
             <canvas className="input-canvas"
                     ref="inputCanvas"
-                    width={300} height={400}
+                    width={224} height={224}
                     onMouseDown={this.onMouseDown}
                     onMouseMove={this.onMouseMove}
                     onMouseOut={this.onMouseUp}
                     onMouseUp={this.onMouseUp}
                     />
               <button className="clear-canvas pure-button" onClick={this.clear}>Clear</button>
+              <canvas className="rescaled-canvas"
+                      ref="rescaledCanvas"
+                      width={28} height={28}/>
           </div>
   }
 };
