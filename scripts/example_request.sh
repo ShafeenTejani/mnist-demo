4dbd25eee417169a1e715a98102e55524cd2b30a
Shafeen Tejani <shafeen.tejani@gmail.com>
2017-01-11 23:42:52 +0000

    Update port

--- scripts/example_request.sh
+++ scripts/example_request.sh
@@ -1,1 +1,1 @@
-curl -H 'Content-Type: application/json' -d @data.json localhost:5000/api/mnist/convolutional
+curl -H 'Content-Type: application/json' -d @data.json localhost:8000/api/mnist/convolutional
